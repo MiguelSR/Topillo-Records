0e392393d567f56fb86c0575f7ab9bbb3e9dc222
MiguelSR <miguelsr87@gmail.com>
2019-11-13 21:49:23 +0100

    Update web

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,17 @@ var fileinclude = require('gulp-file-include');
 var gulp = require('gulp');
 var mustache = require("gulp-mustache");
 
-        //.pipe(mustache('items.json'), {extension: 'html'})
+//.pipe(mustache('items.json'), {extension: 'html'})
 gulp.task('compile', function() {
-    gulp.src('./templates/*.html')
-        .pipe(fileinclude({
-            prefix: '@@',
-            basepath: 'templates',
-        }))
-        .pipe(mustache('items.json'), {'extension': 'html'})
-        .pipe(gulp.dest('.'));
+    return (
+        gulp.src('./templates/*.html')
+            .pipe(fileinclude({
+                prefix: '@@',
+                basepath: 'templates',
+            }))
+            .pipe(mustache('items.json'), {'extension': 'html'})
+            .pipe(gulp.dest('.'))
+    );
 });
 
-gulp.task('default', ['compile',]);
+gulp.task('default', gulp.series('compile'));
